11ef568d5e4618a9e15a2dc62483dd137a07db52
Sungjoon Park <sungjoon512@gmail.com>
2022-08-28 19:38:15 +0900

    nav-drawer to work

--- src/components/templates/default/nav-drawer/index.tsx
+++ src/components/templates/default/nav-drawer/index.tsx
@@ -1,9 +1,15 @@
+import { useState } from 'react'
+
 import { Nav } from './nav'
 import { Drawer } from './drawer'
 
-export const NavDrawer = () => (
-  <>
-    <Nav />
-    <Drawer />
-  </>
-)
+export const NavDrawer = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <>
+      <Nav setIsOpen={setIsOpen} />
+      <Drawer isOpen={isOpen} />
+    </>
+  )
+}

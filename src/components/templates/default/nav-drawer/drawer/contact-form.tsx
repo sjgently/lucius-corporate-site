4f405eb37edf03a1d7fcd6885dd28227e08fa351
Sungjoon Park <sungjoon512@gmail.com>
2023-01-03 23:22:16 +0900

    not used for now

--- src/components/templates/default/nav-drawer/drawer/contact-form.tsx
+++ src/components/templates/default/nav-drawer/drawer/contact-form.tsx
@@ -1,7 +1,8 @@
 import { SubmitHandler, useForm } from 'react-hook-form'
-import { parseOneAddress } from 'email-addresses'
+// import { parseOneAddress } from 'email-addresses'
 
-const getIsValidEmail = (value: string) => parseOneAddress(value) !== null
+// const getIsValidEmail = (value: string) => parseOneAddress(value) !== null
+const getIsValidEmail = (value: string) => value !== null
 
 const buttonBaseClassList = [
   'text-white',
@@ -54,7 +55,7 @@ export default function ContactForm() {
           id='email'
           {...register('email', {
             required: true,
-            validate: (value) => parseOneAddress(value) !== null
+            validate: (value) => getIsValidEmail(value)
           })}
           className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
           placeholder='name@company.com'

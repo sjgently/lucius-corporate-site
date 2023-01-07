29aa688ed0272a0ce396284258fb2336bef9e306
Sungjoon Park <sungjoon512@gmail.com>
2023-01-07 21:48:48 +0900

    do not expose contact form for now

--- src/components/templates/default/nav-drawer/nav/index.tsx
+++ src/components/templates/default/nav-drawer/nav/index.tsx
@@ -1,8 +1,8 @@
 import Logo from '@src/components/atoms/logo'
 
-// import ThemeToggleButton from './theme-toggle-button'
+import ThemeToggleButton from './theme-toggle-button'
 // import LanguageDropdown from './language-dropdown'
-import ContactButton from './contact-button'
+// import ContactButton from './contact-button'
 
 export default function Nav() {
   return (
@@ -10,9 +10,9 @@ export default function Nav() {
       <div className='max-w-screen-xl flex flex-wrap justify-between items-center mx-auto'>
         <Logo />
         <div className='flex gap-4 md:order-2'>
-          {/* <ThemeToggleButton /> */}
+          <ThemeToggleButton />
           {/* <LanguageDropdown /> */}
-          <ContactButton />
+          {/* <ContactButton /> */}
         </div>
       </div>
     </nav>

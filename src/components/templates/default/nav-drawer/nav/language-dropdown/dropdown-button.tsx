fd0744a6160dfb085ba0db86d2e3639bd99483cf
Sungjoon Park <sungjoon512@gmail.com>
2023-01-07 19:31:45 +0900

    import country icons

--- src/components/templates/default/nav-drawer/nav/language-dropdown/dropdown-button.tsx
+++ src/components/templates/default/nav-drawer/nav/language-dropdown/dropdown-button.tsx
@@ -1,3 +1,5 @@
+import CountryIconUs from '@src/components/svgs/country-icon-us'
+
 export default function DropdownButton() {
   return (
     <button
@@ -5,44 +7,7 @@ export default function DropdownButton() {
       data-dropdown-toggle='language-dropdown-menu'
       className='inline-flex items-center justify-center p-2 text-sm text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white'
     >
-      <svg
-        className='w-5 h-5 mr-2 rounded-full'
-        aria-hidden='true'
-        xmlns='http://www.w3.org/2000/svg'
-        xmlnsXlink='http://www.w3.org/1999/xlink'
-        viewBox='0 0 3900 3900'
-      >
-        <path fill='#b22234' d='M0 0h7410v3900H0z' />
-        <path
-          d='M0 450h7410m0 600H0m0 600h7410m0 600H0m0 600h7410m0 600H0'
-          stroke='#fff'
-          strokeWidth='300'
-        />
-        <path fill='#3c3b6e' d='M0 0h2964v2100H0z' />
-        <g fill='#fff'>
-          <g id='d'>
-            <g id='c'>
-              <g id='e'>
-                <g id='b'>
-                  <path
-                    id='a'
-                    d='M247 90l70.534 217.082-184.66-134.164h228.253L176.466 307.082z'
-                  />
-                  <use xlinkHref='#a' y='420' />
-                  <use xlinkHref='#a' y='840' />
-                  <use xlinkHref='#a' y='1260' />
-                </g>
-                <use xlinkHref='#a' y='1680' />
-              </g>
-              <use xlinkHref='#b' x='247' y='210' />
-            </g>
-            <use xlinkHref='#c' x='494' />
-          </g>
-          <use xlinkHref='#d' x='988' />
-          <use xlinkHref='#c' x='1976' />
-          <use xlinkHref='#e' x='2470' />
-        </g>
-      </svg>
+      <CountryIconUs className='w-5 h-5 mr-2 rounded-full' />
       English (US)
     </button>
   )

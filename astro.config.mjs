24221e99e7a0324e77138d3509c419de2f4042ca
Sungjoon Park <sungjoon512@gmail.com>
2023-01-24 23:02:33 +0900

    use the plugins from the jotai/babel bundle

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,14 @@ import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
 import image from '@astrojs/image'
 import mdx from '@astrojs/mdx'
+import jotaiDebugLabel from 'jotai/babel/plugin-debug-label'
+import jotaiReactRefresh from 'jotai/babel/plugin-react-refresh'
 
 // https://astro.build/config
 export default defineConfig({
   // Enable React to support React JSX components.
   integrations: [
-    react(),
+    react({ babel: { plugins: [jotaiDebugLabel, jotaiReactRefresh] } }),
     tailwind(),
     image(),
     mdx({
